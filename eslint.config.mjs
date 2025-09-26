d6a576e53f221124036ae8de5961d0d0272bed10
gamerdoff <macksym.siomak2004@gmail.com>
2025-09-26 21:20:35 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+module.exports = {
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs,jsx}"],
+      plugins: ["js"],
+      extends: ["plugin:js/recommended"],
+      env: {
+        browser: true
+      },
+      globals: {
+        window: true,
+        document: true
+      }
+    }
+  ],
+  plugins: ["react"],
+  extends: ["plugin:react/recommended"]
+};
